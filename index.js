894acc167b9d4ae6a6a87391553f7f87fdcde3a0
piojo2287 <33067911+piojo2287@users.noreply.github.com>
2019-02-12 11:38:52 -0300

    finalizacion de curso de express

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+//middlewares
+app.use(morgan('dev'));
+
+//Requiriendo rutas
+const routes = require("./routes");
+const routesApi = require("./routes-api");
+
+
+//setting
+app.set('appname', 'Mi primer server');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 
 //Routes
-app.get('/', (req, res) => {
- res.end('Hello world');
-});
+app.use(routes);
+app.use("/api", routesApi);
 
-app.get('/login', (req, res) => {
-    res.end('Aqui va el login');
-   });
-   
 app.get('*', (req, res) => {
     res.end('Archivo no encontrado');
-   });
-   
+});
 
 app.listen(3000, function () {
-console.log('Servidor funcionando')
+    console.log('Servidor funcionando')
+    console.log('Nombre de la APP', app.get('appname'))
 })
 
 
+function newFunction() {
+    app.use(morgan('short'));
+    app.use(morgan('tiny'));
+}
 
